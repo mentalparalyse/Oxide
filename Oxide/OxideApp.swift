60fd8c16883c905bd9a864873bb29a14c5f25593
Alexei Savasteev <lexsava@Lexs-MacBook-Pro.local>
2025-09-08 19:51:09 +0300

    Added project files.

---
+++ Oxide/OxideApp.swift
@@ -0,0 +1,17 @@
+//
+//  OxideApp.swift
+//  Oxide
+//
+//  Created by Lex Sava on 08.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct OxideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
