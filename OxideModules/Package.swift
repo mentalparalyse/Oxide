be230aa777c1567a3885f264a04984602fbffd4f
Baluta Eugen <30663151+balutaeugen@users.noreply.github.com>
2025-09-30 00:20:31 +0300

    Update Package.swift
    Try adding macOS support

--- OxideModules/Package.swift
+++ OxideModules/Package.swift
@@ -6,7 +6,8 @@ import PackageDescription
 let package = Package(
     name: "OxideModules",
     platforms: [
-        .iOS(.v16)
+        .iOS(.v16),
+        .macOS(.v10_15) // Added for compatibility
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
